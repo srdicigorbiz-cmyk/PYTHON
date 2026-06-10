ed969d20ddb00823abf781a3af4b2bfdee123970
Igor Srdic <srdic.igor.biz@gmail.com>
2026-06-10 13:38:22 +0200

    0061 - Register Login System Project CODDY

--- 0060 - API/0060 - 006 - API CODDY FACT GENERATOR course.py
+++ 0060 - API/0060 - 006 - API CODDY FACT GENERATOR course.py
@@ -3,17 +3,20 @@ import requests
 import json
 
 def get_fact():
-    response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random", search_for)
     content = response.text
     data = json.loads(content)
     return data['text']
 
-
-
+print("WELCOME TO RANDOM FACT GENERATOR")
+search_for= str(input("Give a word to search fact."))
 num = int(input())
-
-
-counter = 0
-while counter < num:
-    print(get_fact())
-    counter += 1
+if num > 7:
+    print("Maximum number of facts per execution is 7")
+elif num <= 0:
+    print("Minimum number of facts per execution is 1")
+else:
+    counter = 0
+    while counter < num:
+        print(get_fact())
+        counter += 1
