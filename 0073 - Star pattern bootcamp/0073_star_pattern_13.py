d83e9c0fdf2f93e8fd49eedadaf978e7f0679a71
Igor Srdic <srdic.igor.biz@gmail.com>
2026-07-15 18:08:49 +0200

    0073 - star pattern

---
+++ 0073 - Star pattern bootcamp/0073_star_pattern_13.py
@@ -0,0 +1,25 @@
+r = 9
+c = 9
+
+st = "*"
+sp = " "
+
+
+for i in range(r):
+    if i < r // 2 + 1:
+        for j in range(c):
+            if j == c//2-i or j == c//2+i:
+                print(st, end="")
+            else:
+                if j < c//2+i:
+                    print(sp, end = "")
+    
+    if i > r//2:
+        for j in range(c):
+            if j==i-r//2 or j==(c-1)-(i-r//2):
+                print(st, end="")
+            else:
+                if j < (c-1)-(i-r//2):
+                    print(sp, end= "")
+    print()
+   
